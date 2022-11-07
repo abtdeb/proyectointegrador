c0e890577a9d81134eb69f355ef155021e293ced
abtdeb <abt.deborah@gmail.com>
2022-11-07 01:23:45 -0300

    hola

--- scripts.js
+++ scripts.js
@@ -6,19 +6,65 @@ const apellido = document.getElementById("Apellidoform");
 const mail = document.getElementById ("Emailform");
 const cantidad = document.getElementById("Cantticket");
 const botonResumen = document.getElementById("resum");
-const expReg =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ 
+const expReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ 
+
+const CantidadI = document.getElementById("Cantticket");
+const CategSelec = document.getElementById("inputCateg");
+const totalp = document.getElementById("totalp");
 
 function validarT(){
-    if (nombre.value.length < 3) {
+    if (nombre.value.length < 2) {
         alert("Ingrese un nombre valido");
+        return false
     }
     else if (apellido.value.length < 2) {
         alert("Ingrese un apellido valido")
+        return false
     }
     else if(!expReg.test(Emailform.value)){
         alert("Ingrese un Email valido")
+        return false
+    }
+    else if(cantidad.value = 0){
+        alert("Ingrese la cantidad de Tickets")
+        return false
+    }
+    else{
+        return true
     }
 
 }
 
-botonResumen.addEventListener("click", validarT);
+function TotalPago(){
+    let total = 0
+    var valido = validarT();
+    if (valido == true){
+        
+        switch(parseInt(CategSelec.value)){
+            case 1:
+                total= CantidadI.value * 200;
+                totalp.innerHTML = "Total a Pagar " + total;
+                break
+
+            case 2:
+                total = CantidadI.value * (200-(200*(80/100)));
+                totalp.innerHTML = "Total a Pagar " + total;
+                break
+
+            case 3:
+                total = CantidadI.value * (200-(200*(50/100)));
+                totalp.innerHTML = "Total a Pagar " + total;
+                break
+            
+            case 4:
+              total = CantidadI * (200-(200*(15/100)));
+                    totalp.innerHTML = "Total a Pagar " + total;
+                    break
+
+            
+        }
+    }
+}
+
+
+botonResumen.addEventListener("click", totalp);
